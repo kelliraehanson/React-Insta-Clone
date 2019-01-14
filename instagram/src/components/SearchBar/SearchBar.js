cd2fede2f68bcaa46fd732f4a2d145ab3af42a7f
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-14 15:02:40 -0600

    search bar working now need to edit it

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+
+class SearchBar extends Component {
+
+    render() {
+
+      return (
+
+        <div className="SearchBar">
+        <h1>hi</h1>
+         
+        </div>
+
+      );
+    }
+  }
+  
+  export default SearchBar;
+  
